6d54f9e2367139cc3253bfbe586ae7cdcce47dd2
Asgard Kaleb Marroquin <kmarroquin@mail.usf.edu>
2016-06-27 14:13:06 +0200

    loadarraydata didn't transfer with last commit

--- c_port/loadArrayData.h
+++ c_port/loadArrayData.h
@@ -14,16 +14,18 @@
 #define LOADARRAY
 //struct cbArray *loadArrayData(struct1_T tstdat_scalar_struct));
 void loadArrayData(struct1_T tstdat_scalar_struct);
+//struct cbArrayStruct loadArrayData(struct1_T tstdat_scalar_struct);
+
 #endif
 
 
-#ifndef CBARRAYSTRUCT 	// header guard for cbArrayStruct
-#define CBARRAYSTRUCT
-struct cbArrayStruct {
-		struct3_T tst_array_struct;
-		struct4_T fzg_array_struct;
-};
-#endif
+//#ifndef CBARRAYSTRUCT 	// header guard for cbArrayStruct
+//#define CBARRAYSTRUCT
+//struct cbArrayStruct {
+//		struct3_T tst_pointer;
+//		struct4_T fzg_pointer;
+//};
+//#endif
 
 
 #ifndef VARSTRUCT		// header guard for varStruct
